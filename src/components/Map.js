2de31145d9acf297bed3899a3af7395ce2fcc291
Emox133 <emir.betaacc0unt@gmail.com>
2021-05-23 15:56:13 +0200

    max bounds

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,8 +5,12 @@ import Marker from './Marker'
 // import Circle from './Circle'
 
 const Map = () => {
+    const northEastBounds = [44.49589310341624, 18.781642913818363]
+    const southWestBounds = [44.40275975498712, 18.517971038818363]
+    const bounds = [northEastBounds, southWestBounds]
+
     return (
-        <MapContainer center={[44.44929, 18.64978]} zoom={15} minZoom={13} >
+        <MapContainer center={[44.44929, 18.64978]} zoom={15} minZoom={13} maxBounds={bounds}>
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
