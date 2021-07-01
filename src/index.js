652648909552e977ff5f70eec335082cb55bb1e8
Emox133 <emir.betaacc0unt@gmail.com>
2021-07-01 14:14:55 +0200

    testing our service worker

--- src/index.js
+++ src/index.js
@@ -34,12 +34,12 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
-// if('serviceWorker' in navigator) {
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker.register('./sw.js')
-//     .then(reg => console.log(reg.scope, 'Service worker registered...'))
-//     .catch(err => console.log(err, 'Service worker registration failed...'))
-//   })
-// }
+if('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./sw.js')
+    .then(reg => console.log(reg.scope, 'Service worker registered...'))
+    .catch(err => console.log(err, 'Service worker registration failed...'))
+  })
+}
 
 reportWebVitals();
